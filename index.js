43dea6e5716f42ee987a61de4ce54e17429b0441
Rishi Vedula <126549146+Rishi-Vedula2099@users.noreply.github.com>
2025-03-23 14:56:20 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const socketio = require("socket.io");
+const http = require("http");
+const path = require('path');
+const PORT = 3000;
+
+
+
+const app = express();
+const server = http.createServer(app)
+const io = socketio(server)
+
+
+app.set("view engine", "ejs")
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static('public'));
+
+
+io.on("connection" , function (socket){
+    console.log("connected");
+    socket.on("send-location", function (data){
+        io.emit("receive-location",{
+            id:socket.id,
+            ...data,
+        })
+    })
+});
+
+
+
+
+app.get("/", function (req, res){
+    res.render("index")
+});
+
+
+server.listen(3000,()=>{
+    console.log("Server Start");
+});
