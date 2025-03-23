f0f2b1cf60588ba2dfbcfddb1e89e0c180b2500c
Rishi Vedula <126549146+Rishi-Vedula2099@users.noreply.github.com>
2025-03-23 17:59:39 +0530

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -21,5 +21,18 @@ const map = L.map("map").setView([0, 0], 10);
 
 L.tileLayer("https://a.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
 
+const markers = {};
+
+socket.on("receive-location", (data) => {
+    const { id, latitude, longitude } = data;
+    map.setView([latitude, longitude], 15);
+    if (markers[id]) {
+    // Correct method name to update marker location
+    markers[id].setLatLng([latitude, longitude]);
+} else {
+    // Create a new marker for each unique user
+    markers[id] = L.marker([latitude, longitude]).addTo(map);
+}
+});
 
 
